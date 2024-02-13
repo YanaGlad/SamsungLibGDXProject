f22c3e5774d80be8606ea34a302c443252d77561
y.gladkikh <y.gladkikh@tinkoff.ru>
2024-02-13 18:27:41 +0300

    Massiv

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -6,6 +6,8 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
+import java.util.Random;
+
 public class MyGdxGame extends ApplicationAdapter {
     SpriteBatch batch;
     int SCREEN_WIDTH;
@@ -14,27 +16,36 @@ public class MyGdxGame extends ApplicationAdapter {
     int xStart = 0;
     int yStart = 0;
 
-    MovingImage movingImage;
-    MovingImage movingImage2;
+    MovingImage[] movingImages;
+
+    Random random;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
+        random = new Random();
 
-        movingImage = new MovingImage();
-        movingImage2 = new MovingImage();
+        movingImages = new MovingImage[10];
+        movingImages[0] = new MovingImage();
+        movingImages[0].img = new Texture("badlogic.jpg");
+        movingImages[0].speed = 5;
 
-        movingImage.img = new Texture("badlogic.jpg");
-        movingImage2.img = new Texture("badlogic.jpg");
+        movingImages[1] = new MovingImage();
+        movingImages[1].img = new Texture("badlogic.jpg");
+        movingImages[1].speed = 1;
+        movingImages[1].x = movingImages[1].getEdgePointX(SCREEN_WIDTH, movingImages[1].img);
+        movingImages[1].y = movingImages[1].getEdgePointY(SCREEN_HEIGHT, movingImages[1].img);
 
-        movingImage.speed = 5;
-        movingImage2.speed = 1;
+        for (int i = 2; i < movingImages.length; i++) {
+            movingImages[i] = new MovingImage();
+            movingImages[i].img = new Texture("badlogic.jpg");
+            movingImages[i].speed = random.nextInt(1,  5);
+            movingImages[i].x = random.nextInt(200);
+            movingImages[i].y = random.nextInt(300);
+        }
 
         SCREEN_WIDTH = Gdx.graphics.getWidth();
         SCREEN_HEIGHT = Gdx.graphics.getHeight();
-
-        movingImage2.x = movingImage2.getEdgePointX(SCREEN_WIDTH, movingImage2.img);
-        movingImage2.y = movingImage2.getEdgePointY(SCREEN_HEIGHT, movingImage2.img);
     }
 
     @Override
@@ -42,15 +53,12 @@ public class MyGdxGame extends ApplicationAdapter {
         ScreenUtils.clear(0, 0, 1, 1); ///rgb - red, green, blue, a - alhpa
         batch.begin();
 
-        batch.draw(movingImage.img, movingImage.x, movingImage.y);
-        batch.draw(movingImage2.img, movingImage2.x, movingImage2.y);
+        for (int i = 0; i < movingImages.length; i++) {
+            batch.draw(movingImages[i].img, movingImages[i].x, movingImages[i].y);
 
-        movingImage.moveX(SCREEN_WIDTH, xStart);
-        movingImage.moveY(SCREEN_HEIGHT, yStart);
-
-        movingImage2.moveX(SCREEN_WIDTH, xStart);
-        movingImage2.moveY(SCREEN_HEIGHT, yStart);
-
+            movingImages[i].moveX(SCREEN_WIDTH, xStart);
+            movingImages[i].moveY(SCREEN_HEIGHT, yStart);
+        }
         batch.end();
     }
 
@@ -58,6 +66,8 @@ public class MyGdxGame extends ApplicationAdapter {
     @Override
     public void dispose() {
         batch.dispose();
-        movingImage.img.dispose();
+        for (int i = 0; i < movingImages.length; i++) {
+            movingImages[i].img.dispose();
+        }
     }
 }
